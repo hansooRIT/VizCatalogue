3e6b211a5abcea2950870fbd57c8ef12a5540d35
HanSoo Lee <hxl9400@g.rit.edu>
2018-11-14 00:50:15 -0500

    First 6 graphs made

--- graphs/DonutChart/main.js
+++ graphs/DonutChart/main.js
@@ -1,3 +1,75 @@
-window.onload = function() {
-  // do d3 stuff here...
+
+function createDonutChart() {
+  // dataset for this pie chart
+  let dataset = [
+      {name: "Steak", value: 43 }, 
+      {name: "Chicken", value: 38}, 
+      {name: "Tofu", value: 6}, 
+      {name: "Veggies", value: 10}, 
+      {name: "Ground beef", value:25},
+      {name: "Pork", value: 23}, 
+      {name: "Shrimp", value: 32}]; 
+
+  // d3.pie will sort our values, but it does so in a way that 
+  // makes the color scale mapping hard to predict, so it's easier
+  // to sort ourselves
+  dataset.sort((a,b) => b.value - a.value);
+
+  // settings for our pie chart 
+  let w = 600;
+  let h = 500;
+  let innerRadius = h/2 - 50;       
+  let outerRadius = h/2;
+
+  // create our SVG element
+  let svg = d3
+    .select("#donutChart")
+    .attr("width", w)
+    .attr("height", h);
+
+  // create D3 pie layout that converts 
+  // dataset into one appropriate for pie charts 
+  let pie = d3.pie()
+              .value(d => d.value);       
+
+  // create D3 arc generator we will use for pie layout 
+  let arc = d3.arc()
+              .innerRadius(innerRadius)
+              .outerRadius(outerRadius);
+
+
+  // use a color scale for the bar colors
+  // ordinal scales created given an array of output range values
+  // usually used by giving input indices into array 
+  let cScale = d3.scaleOrdinal(d3.schemeCategory10);
+
+  console.table(pie(dataset));
+
+  let arcs = 
+    svg.selectAll('g.arc')
+        .data(pie(dataset))
+        .enter()
+        .append('g')
+          .attr('class', 'arc')
+          .attr('transform', `translate(${h/2}, ${h/2})`);
+ 
+  // append an SVG path to each g element for the pie wedge
+  // uses the arc generator we configured earlier 
+  arcs.append('path')
+    .attr('fill', (d,i) => cScale(i))
+    .attr('d', arc)
+    .append('title')
+      .text(d => d.data.name);
+
+  // now append text elements to each 'g' pie wedge
+  arcs.append('text')
+    .attr('transform', d => `translate(${arc.centroid(d)})`)
+    .attr('text-anchor', 'middle')
+    .text(d => d.value);
 }
+
+window.onload = function() {
+  // console.table is very handy for viewing row/column data
+  createDonutChart();
+};
+          
--- graphs/ScatterPlot/main.js
+++ graphs/ScatterPlot/main.js
@@ -1,3 +1,66 @@
+let dataset;
+
+function rowConverter(d) {
+    return {
+        startingSalary: parseFloat(d.startingSalary),
+        gpa: parseFloat(d.gpa)
+    };
+}
+
+function createScatterplot() {
+    let w = 600;
+    let h = 500;
+    
+    let gpaMin = d3.min(dataset, (d) => d.gpa);
+    let gpaMax = d3.max(dataset, (d) => d.gpa);
+    let salaryMin = d3.min(dataset, (d) => d.startingSalary);
+    let salaryMax = d3.max(dataset, (d) => d.startingSalary);
+    
+    let svg = d3
+        .select("#scatterPlot")
+        .attr("width", w)
+        .attr("height", h);
+    
+    let yScale = d3
+        .scaleLinear()
+        .domain([salaryMin, 100000])
+        .range([h - 20, 20]);
+                                
+    let xScale = d3
+        .scaleLinear()
+        .domain([gpaMin, 4.0])
+        .range([50, w - 20]);
+    
+    svg
+        .selectAll("circles")
+        .data(dataset)
+        .enter()
+        .append("circle")
+        .attr('cx', (d) => xScale(d.gpa))
+        .attr('cy', (d) => yScale(d.startingSalary))
+        .attr('fill', 'blue')
+        .attr('r', 5);
+    
+    let xAxis = d3
+        .axisBottom(xScale);
+
+    let xAxisGroup = svg
+        .append("g")
+        .attr("transform", `translate(0, ${h - 20})`)
+        .call(xAxis);
+    
+    let yAxis = d3.axisLeft(yScale);
+
+    let yAxisGroup = svg
+        .append("g")
+        .attr("transform", `translate(50, 0)`)
+        .call(yAxis);
+
+}
+
 window.onload = function() {
-  // do d3 stuff here...
+  d3.csv("salaryData.csv", rowConverter).then(data => {
+      dataset = data;
+      createScatterplot();
+  })
 }
